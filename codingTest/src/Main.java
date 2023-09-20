2c543960f106d667b72e27ef4773bbddbd31b008
zios <khyunwook0707@gmail.com>
2023-09-20 17:42:38 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 주사위 게임 2
    
    정크 코드 된 감이 있긴함. 그냥 풀음.

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -2,22 +2,28 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(3, 4, new boolean[]{true, false, false, true, true}));
+        System.out.println(solution.solution(2, 6, 1) == 9);
+        System.out.println(solution.solution(5, 3, 3) == 473);
+        System.out.println(solution.solution(4, 4, 4) == 110592);
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181931 코딩테스트 연습 > 코딩 기초 트레이닝 > 등차수열의 특정한 항만 더하기
-    /**                                                                                                                           */
-    public int solution(int a, int d, boolean[] included) {
-        int answer = 0;
-        for (boolean b: included) {
-            if(b) answer += a;
-            a += d;
-        }
-
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181930 코딩테스트 연습 > 코딩 기초 트레이닝 > 주사위 게임 2
+    /**                                                                                                              */
+    public int solution(int a, int b, int c) {
+        int answer = a + b + c;
+        int a1 = a * a;
+        int a2 = a1 * a;
+        int b1 = b * b;
+        int b2 = b1 * b;
+        int c1 = c * c;
+        int c2 = c1 * c;
+        if(a == b && b == c) answer *= (a1 + b1 + c1) * (a2 + b2 + c2);
+        else if(a != b && b != c && a != c) {}
+        else answer *= a1 + b1 + c1;
         return answer;
     }
 }
