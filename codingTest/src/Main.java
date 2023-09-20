14c05da4bc593e93a078572befc07f7718ac231a
zios <khyunwook0707@gmail.com>
2023-09-20 18:31:32 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 원소들의 곱과 합
    
    간단하게 forEach 문 그리고 Math.pow 함수 이용해서 풀어버리기

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,29 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(2, 6, 1) == 9);
-        System.out.println(solution.solution(5, 3, 3) == 473);
-        System.out.println(solution.solution(4, 4, 4) == 110592);
+        System.out.println(solution.solution(new int[]{3, 4, 5, 2, 1}) == 0);
+        System.out.println(solution.solution(new int[]{5, 7, 8, 3}) == 1);
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181930 코딩테스트 연습 > 코딩 기초 트레이닝 > 주사위 게임 2
-    /**                                                                                                              */
-    public int solution(int a, int b, int c) {
-        int answer = a + b + c;
-        int a1 = a * a;
-        int a2 = a1 * a;
-        int b1 = b * b;
-        int b2 = b1 * b;
-        int c1 = c * c;
-        int c2 = c1 * c;
-        if(a == b && b == c) answer *= (a1 + b1 + c1) * (a2 + b2 + c2);
-        else if(a != b && b != c && a != c) {}
-        else answer *= a1 + b1 + c1;
-        return answer;
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181929 코딩테스트 연습 > 코딩 기초 트레이닝 > 원소들의 곱과 합
+    /**                                                                                                                */
+    public int solution(int[] num_list) {
+        int a = Arrays.stream(num_list).sum();
+        int b = 1;
+        for(int i: num_list) b *= i;
+        System.out.println(Math.pow(a, 2) + ", " + b);
+        if(Math.pow(a, 2) < b) return 1;
+        return 0;
     }
 }
