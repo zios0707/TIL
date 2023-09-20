31739ff2a2b25635034d9390f043dc2ddde80d59
zios <khyunwook0707@gmail.com>
2023-09-20 19:40:26 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 수 조작하기 2
    
    해쉬 맵 쓰면 빨라진다는데 일단 수 조작 1 처럼 스위치 케이스 씀

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -2,35 +2,41 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(0, "wsdawsdassw") == -1);
+        System.out.println(solution.solution(new int[]{0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1}).equals("wsdawsdassw"));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181926 코딩테스트 연습 > 코딩 기초 트레이닝 > 수 조작하기 1
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181925 코딩테스트 연습 > 코딩 기초 트레이닝 > 수 조작하기 2
     /**                                                                                                                */
-    public int solution(int n, String control) {
-        int answer = n;
-        for (char c:
-             control.toCharArray()) {
-            switch (c) {
-                case 'w':
-                    answer += 1;
-                    break;
-                case 'a':
-                    answer -= 10;
-                    break;
-                case 's':
-                    answer -= 1;
-                    break;
-                case 'd':
-                    answer += 10;
-                    break;
+    public String solution(int[] numLog) {
+        String answer = "";
+        Integer tmp = null;
+        for (int i:
+             numLog) {
+            if(tmp != null) {
+                int gap = i - tmp;
+                switch (gap) {
+                    case 1:
+                        answer += "w";
+                        break;
+                    case 10:
+                        answer += "d";
+                        break;
+                    case -1:
+                        answer += "s";
+                        break;
+                    case -10:
+                        answer += "a";
+                        break;
+                }
             }
-
+            tmp = i;
+            System.out.println(answer);
         }
+
         return answer;
     }
 }
