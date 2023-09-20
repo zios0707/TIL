9da223ae79bbff668f55274152f9b6929af5cca8
zios <khyunwook0707@gmail.com>
2023-09-20 19:59:58 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 수열과 구간 쿼리 3
    
    forEach 문으로 1차원 배열로 만들어주고 인덱스 0, 1에서 값을 꺼내서 arr 배열에서 스왑만 시켜주면 됨

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -2,41 +2,21 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1}).equals("wsdawsdassw"));
+        System.out.println(solution.solution(new int[]{0, 1, 2, 3, 4}, new int[][]{{0, 3}, {1, 2}, {1, 4}}));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181925 코딩테스트 연습 > 코딩 기초 트레이닝 > 수 조작하기 2
-    /**                                                                                                                */
-    public String solution(int[] numLog) {
-        String answer = "";
-        Integer tmp = null;
-        for (int i:
-             numLog) {
-            if(tmp != null) {
-                int gap = i - tmp;
-                switch (gap) {
-                    case 1:
-                        answer += "w";
-                        break;
-                    case 10:
-                        answer += "d";
-                        break;
-                    case -1:
-                        answer += "s";
-                        break;
-                    case -10:
-                        answer += "a";
-                        break;
-                }
-            }
-            tmp = i;
-            System.out.println(answer);
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181924 코딩테스트 연습 > 코딩 기초 트레이닝 > 수열과 구간 쿼리 3
+    /**                                                                                                                  */
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] row: queries) {
+            int tmp = arr[row[0]];
+            arr[row[0]] = arr[row[1]];
+            arr[row[1]] = tmp;
         }
-
-        return answer;
+        return arr;
     }
 }
