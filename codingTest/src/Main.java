7134257abf417571464a37a62409ac65589e4ecd
zios <khyunwook0707@gmail.com>
2023-09-20 18:50:51 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 이어 붙인 수
    
    String.valueOf() 랑 Integer.parseInt() 함수를 사용해서 문자열과 정수로 왔다리 갔다리 해서 풀음

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,25 +1,26 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{3, 4, 5, 2, 1}) == 0);
-        System.out.println(solution.solution(new int[]{5, 7, 8, 3}) == 1);
+        System.out.println(solution.solution(new int[]{3, 4, 5, 2, 1}) == 393);
+        System.out.println(solution.solution(new int[]{5, 7, 8, 3}) == 581);
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181929 코딩테스트 연습 > 코딩 기초 트레이닝 > 원소들의 곱과 합
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181928 코딩테스트 연습 > 코딩 기초 트레이닝 > 이어 붙인 수
     /**                                                                                                                */
     public int solution(int[] num_list) {
-        int a = Arrays.stream(num_list).sum();
-        int b = 1;
-        for(int i: num_list) b *= i;
-        System.out.println(Math.pow(a, 2) + ", " + b);
-        if(Math.pow(a, 2) < b) return 1;
-        return 0;
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        for(Integer i: num_list) {
+            if(i % 2 == 0) even += String.valueOf(i);
+            else odd += String.valueOf(i);
+        }
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        return answer;
     }
 }
