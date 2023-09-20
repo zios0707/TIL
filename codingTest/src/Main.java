063ded894c5fdee9d4f3020c8a4c860ec346c595
zios <khyunwook0707@gmail.com>
2023-09-20 19:21:12 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 마지막 두 원소
    
    받은 배열에 크기를 1 늘린 새 배열을 만들어서 값을 넣어서 리턴시킴

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -2,25 +2,26 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{3, 4, 5, 2, 1}) == 393);
-        System.out.println(solution.solution(new int[]{5, 7, 8, 3}) == 581);
+        System.out.println(solution.solution(new int[]{2, 1, 6}));
+        System.out.println(solution.solution(new int[]{5, 2, 1, 7, 5}).toString());
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181928 코딩테스트 연습 > 코딩 기초 트레이닝 > 이어 붙인 수
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181927 코딩테스트 연습 > 코딩 기초 트레이닝 > 마지막 두 원소
     /**                                                                                                                */
-    public int solution(int[] num_list) {
-        int answer = 0;
-        String odd = "";
-        String even = "";
-        for(Integer i: num_list) {
-            if(i % 2 == 0) even += String.valueOf(i);
-            else odd += String.valueOf(i);
+    public int[] solution(int[] list) {
+        int length = list.length - 1;
+        int[] array = new int[length + 2];
+        System.arraycopy(list, 0, array, 0, list.length);
+        if (list[length] > list[length - 1]) {
+            array[length + 1] = list[length] - list[length - 1];
+        } else {
+            array[length + 1] = list[length] * 2;
         }
-        answer = Integer.parseInt(even) + Integer.parseInt(odd);
-        return answer;
+
+        return array;
     }
 }
