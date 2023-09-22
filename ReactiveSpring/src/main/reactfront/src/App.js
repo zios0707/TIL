70212ab3e57a11438efb0bfbd1c4c7c3cebac226
zios <khyunwook0707@gmail.com>
2023-09-22 16:53:01 +0900

    리액트 // 함수 분리
    함수를 서로 분리 시켜서 컴포넌트를 여러개로 쪼개버림.

--- ReactiveSpring/src/main/reactfront/src/App.js
+++ ReactiveSpring/src/main/reactfront/src/App.js
@@ -1,32 +1,44 @@
 import "./App.css";
-import {useState} from "react";
+import { useState } from "react";
 
-function App() {
-
-    const [num, setNum] = useState(0);
-
-    const [numList, setNumList] = useState([]);
-
-    function numRecoding() {
-        setNumList([...numList, num]);
-
-        setNum(0);
-    }
+const RecordForm = ({ numList, setNumList }) => {
+  const [num, setNum] = useState(0);
 
   return (
-    <div className="App">
-        <div>현재 숫자 : {num}</div>
-        <button onClick={() => setNum(num + 1)}>숫자 증가</button>
-        <button onClick={() => setNum(num - 1)}>숫자 감소</button>
-        <button onClick={numRecoding} >숫자 기록</button>
-        <h1>저장된 숫자</h1>
-        <ul>
-            {numList.map((num) => (
-                <li>{num}</li>
-            ))}
-        </ul>
+    <div>
+      <div>현재 숫자 : {num}</div>
+      <button onClick={() => setNum(num + 1)}>숫자 증가</button>
+      <button onClick={() => setNum(num - 1)}>숫자 감소</button>
+      <button onClick={() => setNum(0)}>숫자 초기화</button>
+      <hr />
+      <button onClick={() => setNumList([...numList, num])}>숫자 기록</button>
+      <button onClick={() => setNumList([])}>기록 초기화</button>
     </div>
   );
-}
+};
+
+const RecordList = ({ numList }) => {
+  console.log(numList);
+  return (
+    <div>
+      <h2>기록된 숫자</h2>
+      {numList.length > 0 ? (
+        numList.map((num) => <div>{num}</div>)
+      ) : (
+        <div>기록 없음</div>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  const [numList, setNumList] = useState([]);
+  return (
+    <div style={{ margin: "40px auto", width: "800px", textAlign: "center" }}>
+      <RecordForm numList={numList} setNumList={setNumList} />
+      <RecordList numList={numList} />
+    </div>
+  );
+};
 
 export default App;
