cbdcdd0f2dcf232924676d819b58e02072c7995a
zios <khyunwook0707@gmail.com>
2023-09-22 17:28:58 +0900

    리액트 // if 문을 이용한 input 태그
    기존 html의 input 태그와 달리 기본 설정을 해줘야 되고 그 점을 이용해서 수정 버튼을 눌러 수정이 동적으로 이루어지게 할 수 있다.

--- ReactiveSpring/src/main/reactfront/src/App.js
+++ ReactiveSpring/src/main/reactfront/src/App.js
@@ -1,44 +1,33 @@
 import "./App.css";
 import { useState } from "react";
 
-const RecordForm = ({ numList, setNumList }) => {
-  const [num, setNum] = useState(0);
-
-  return (
-    <div>
-      <div>현재 숫자 : {num}</div>
-      <button onClick={() => setNum(num + 1)}>숫자 증가</button>
-      <button onClick={() => setNum(num - 1)}>숫자 감소</button>
-      <button onClick={() => setNum(0)}>숫자 초기화</button>
-      <hr />
-      <button onClick={() => setNumList([...numList, num])}>숫자 기록</button>
-      <button onClick={() => setNumList([])}>기록 초기화</button>
-    </div>
-  );
-};
-
-const RecordList = ({ numList }) => {
-  console.log(numList);
-  return (
-    <div>
-      <h2>기록된 숫자</h2>
-      {numList.length > 0 ? (
-        numList.map((num) => <div>{num}</div>)
-      ) : (
-        <div>기록 없음</div>
-      )}
-    </div>
-  );
-};
-
 const App = () => {
-  const [numList, setNumList] = useState([]);
-  return (
-    <div style={{ margin: "40px auto", width: "800px", textAlign: "center" }}>
-      <RecordForm numList={numList} setNumList={setNumList} />
-      <RecordList numList={numList} />
+  const [text, setText] = useState("1");
+  const [edit, setEdit] = useState("false");
+
+  let content = (
+    <div>
+      {text}
+      <button onClick={() => setEdit(true)}>수정</button>
     </div>
   );
+
+  if (edit) {
+    content = (
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button onClick={() => setEdit(false)}>수정</button>
+      </div>
+    );
+  }
+
+  return <>{content}</>;
 };
 
 export default App;
