4fb186cb6de3e17edbff03a9bd1a918ef8f19add
zios <khyunwook0707@gmail.com>
2023-09-24 23:04:48 +0900

    리액트 //  axios 로 api 통신
    대충 받는 방법이 세가지 있는데 두번째 방법이 제일 간단하고 유용함.
    그 두번째 방법을 대충 정리하자면
    useEffect 함수 내에서 axios호출 후 해당 api의 요청 형식에 따른 메서드 "get"을 불러옴.
    그리고 해당 매개변수 칸에 통신을 받을 도메인을 넣음.
    .then 메서드 호출 후 매개변수 칸에서 통신으로 받은 정보를 사용하면 됨.

--- ReactiveSpring/src/main/reactfront/src/App.js
+++ ReactiveSpring/src/main/reactfront/src/App.js
@@ -1,33 +1,34 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [text, setText] = useState("1");
-  const [edit, setEdit] = useState("false");
+  const [posts, setPosts] = useState([]);
 
-  let content = (
-    <div>
-      {text}
-      <button onClick={() => setEdit(true)}>수정</button>
-    </div>
+  useEffect(() => {
+    /*axios({ 방법 1
+      method:"GET",
+      url:'https://jsonplaceholder.typicode.com/photos'
+    }).then(response => setPosts(response.data));*/
+
+    axios.get('https://jsonplaceholder.typicode.com/photos') /*방법 2*/
+        .then(response => setPosts(response.data))
+
+
+    /*const response = await axios.get('https://jsonplaceholder.typicode.com/photos') 방법 3 비동기 형식으로 한다는데 컴파일 오류 뜸
+    setPosts(response.data)*/
+  });
+
+  return (
+      <ul>
+        {posts.map(post => (
+            <li key={post.id}>
+              <div>{post.title}</div>
+              <div><img src={post.thumbnailUrl} alt=""/></div>
+            </li>
+        ))}
+      </ul>
   );
-
-  if (edit) {
-    content = (
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-        <button onClick={() => setEdit(false)}>수정</button>
-      </div>
-    );
-  }
-
-  return <>{content}</>;
 };
 
 export default App;
