c6fd2d9be79fc38cbea0fcaac0057a3c2dd24cc2
zios <khyunwook0707@gmail.com>
2023-10-04 20:31:10 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 주사위 게임 3
    
    노가다로 풀음. 어떻게든 반례를 찾아서 해결 해 냈다.

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,21 +1,76 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(false, true, true, true));
+        System.out.println(solution.solution(1, 1, 1, 6));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181917 코딩테스트 연습 > 코딩 기초 트레이닝 > 간단한 논리 연산
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181916 코딩테스트 연습 > 코딩 기초 트레이닝 > 주사위 게임 3
     /**                                                                                                                */
-    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = (x1 || x2) && (x3 || x4);
-        return answer;
+    public int solution(int a, int b, int c, int d) {
+        int[] point = new int[7];
+        int count;
+        int[] query;
+        point[a]++;
+        point[b]++;
+        point[c]++;
+        point[d]++;
+
+        if(point[a] == 4) { // (4)
+
+            count = 4;
+            query = new int[]{a, 0};
+
+        }else if(point[a] == 3 || point[b] == 3){ // (3), (1)
+
+            count = 3;
+            int index = 0;
+            if(point[a] == 1) index = a;
+            else if (point[b] == 1) index = b;
+            else if (point[c] == 1) index = c;
+            else if (point[d] == 1) index = d;
+            int sec;
+
+            if (index == a) sec = b;
+            else sec = a;
+            query = new int[]{index, sec};
+
+        }else if (a == b || a == c || a == d || b == c || b == d || c == d) {
+            int[] arr = new int[2];
+            if(point[a] == 1 || point[b] == 1 || point[c] == 1) { // 2, (1), (1)
+                count = 1;
+                int Iter = 0;
+                for(int i = 1; i <= 6; i++)
+                    if(point[i] == 1) arr[Iter++] = i;
+            }else { // (2), (2)
+                count = 2;
+                if(a == b) arr = new int[]{a, c};
+                else arr = new int[]{a, b};
+
+            }
+            query = arr;
+        }else { // 1, 1, 1, (1) <= 최소값
+            count = 0;
+            int[] arr = new int[]{a, b, c, d};
+            int min = Arrays.stream(arr).min().getAsInt();
+            query = new int[]{min, 0};
+        }
+        // 값 계산은 끝. 이제 정답을 도출하자.
+        int result = switch (count) {
+            case 0 -> query[0];
+            case 1 -> query[0] * query[1];
+            case 2 -> (query[0] + query[1]) * Math.abs(query[0] - query[1]);
+            case 3 -> (int) Math.pow(10 * query[1] + query[0], 2);
+            case 4 -> 1111 * query[0];
+            default -> 0;
+        };
+        System.out.println(count);
+        return result;
     }
 }
