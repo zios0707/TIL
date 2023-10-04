d3e71777eec041a487d292a566b6f0cec10996a0
zios <khyunwook0707@gmail.com>
2023-10-04 17:06:38 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 수열과 구간 쿼리 2
    
    List 자료형 쓰고 싶어서 형변환 하고 이것 저것 해봤는데 자꾸 이상해져서 깡으로 품. 포문 폭탄이라 굉장히 난해하고 이해하기 힘듦.

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Solution solution = new Solution();
 
         System.out.println(solution.solution(new int[]{0, 1, 2, 4, 3}, new int[][]{{0, 4, 2}, {0, 3, 2}, {0, 2, 2}}));
-    }s
+    }
 }
 
 
@@ -12,6 +12,42 @@ class Solution {
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181923 코딩테스트 연습 > 코딩 기초 트레이닝 > 수열과 구간 쿼리 2
     /**                                                                                                                  */
     public int[] solution(int[] arr, int[][] queries) {
+        int[] List = new int[1000];
+        int Iter = 0;
+        for (int[] in: queries
+             ) {
+            int first = in[0], last = in[1], much = in[2];
+            int val = 1000001;
+            for(int i = first; i <= last; i++) {
+                if(arr[i] == -1) {
+                    last++;
+                    if(last == arr.length) {
+                        break;
+                    }
+                }
+                else if(much < arr[i] && arr[i] < val) {
+                    val = arr[i];
+                }
+            }
+            if(val == 1000001) {
+                List[Iter++] = -1;
+            }else {
+                List[Iter++] = val;
+            }
 
+        }
+
+
+        int[] result = new int[Iter];
+        for (int i = 0; i < Iter; i++) {
+            result[i] = List[i];
+        }
+
+        for (int i:result
+             ) {
+            System.out.println(i);
+        }
+
+        return result;
     }
 }
