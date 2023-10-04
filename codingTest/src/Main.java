f4386bce7162c315cdf793f2b912f2548e62b523
zios <khyunwook0707@gmail.com>
2023-10-04 18:50:42 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 콜라츠 수열 만들기
    
    리스트 활용해봄. 확실히 이터레이터 안쓰니까 조금 더 깔끔해 보이는 듯. 근데 나는 아직도 리스트에서 인덱스 접근이 함수로 해야된다는게 이해 안됨.

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,27 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(3, 10));
+        System.out.println(solution.solution(10));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181920 코딩테스트 연습 > 코딩 기초 트레이닝 > 카운트 업
-    /**                                                                                                            */
-    public int[] solution(int start_num, int end_num) {
-        Integer[] integers = new Integer[50];
-        int Iter = 0;
-        for(int i = start_num; i <= end_num; i++) {
-            integers[Iter++] = i;
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181919 코딩테스트 연습 > 코딩 기초 트레이닝 > 콜라츠 수열 만들기
+    /**                                                                                                                  */
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(n);
+        while (n != 1) {
+            if(n % 2 == 0) n /= 2;
+            else n = n * 3 + 1;
+            list.add(n);
         }
-        int[] arr = new int[Iter];
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
 
-        for(int i = 0; i < Iter; i++) {
-            arr[i] = integers[i];
-        }
-        return arr;
+        return result;
     }
 }
