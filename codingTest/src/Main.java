c6f1aee7af5b6c07478a6248e06f425116b46025
zios <khyunwook0707@gmail.com>
2023-10-04 17:28:06 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 배열 만들기 2
    
    간단하게 문자열로 변환해서 5, 0 외 함유 여부를 물어보는 로직.

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -2,32 +2,47 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{0, 1, 2, 4, 3}, new int[][]{{0, 4, 1}, {0, 3, 2}, {0, 3, 3}}));
+        System.out.println(solution.solution(10, 20));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181922 코딩테스트 연습 > 코딩 기초 트레이닝 > 수열과 구간 쿼리 4
-    /**                                                                                                                  */
-    public int[] solution(int[] arr, int[][] queries) {
-        for (int[] in: queries
-             ) {
-            int first = in[0], last = in[1], much = in[2];
-            for(int i = first; i <= last; i++) {
-                if(i % much == 0) {
-                    arr[i]++;
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181921 코딩테스트 연습 > 코딩 기초 트레이닝 > 배열 만들기
+    /**                                                                                                            */
+    public int[] solution(int l, int r) {
+        int[] arr = new int[100];
+        int Iter = 0;
+        boolean a = true;
+        for (int i = l; i <= r; i++) {
+            String str = String.valueOf(i);
+            int count = 0;
+            for (char c: str.toCharArray()
+                 ) {
+                if(!(c == '5' || c == '0')) {
+                    count = 1;
                 }
-                System.out.print(arr[i]);
             }
-            System.out.println("");
+            if(count == 0) {
+                arr[Iter++] = Integer.valueOf(str);
+            }
+        }
 
+        if(Iter == 0) {
+            Iter = 1;
+            a = false;
         }
-        for (int i: arr
-             ) {
-            System.out.print(i);
+        int[] result = new int[Iter];
+
+        if(a) {
+            for(int i = 0; i < Iter; i++) {
+                result[i] = arr[i];
+            }
+        }else {
+            result[0] = -1;
         }
-        return arr;
+
+        return result;
     }
 }
