ffc02c81c4ee131bff408c35a63100c699650a8a
zios <khyunwook0707@gmail.com>
2023-10-04 19:23:26 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 배열 만들기 4
    
    List<Integer> -> int[] 와 같은 엄청난 형변환 같은건 많은 고찰을 해봐야겠다. 바꿀 수 있는 수단은 많지만 효율성을 챙긴 변환 방법은 눈에 띄지 않는다... stream 같은건 뭘까

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -5,28 +5,28 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(10));
+        System.out.println(solution.solution(new int[]{1, 4, 2, 5, 3}));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181919 코딩테스트 연습 > 코딩 기초 트레이닝 > 콜라츠 수열 만들기
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181918 코딩테스트 연습 > 코딩 기초 트레이닝 > 배열 만들기 4
     /**                                                                                                                  */
-    public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        list.add(n);
-        while (n != 1) {
-            if(n % 2 == 0) n /= 2;
-            else n = n * 3 + 1;
-            list.add(n);
-        }
-        int[] result = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int Iter = 0;
+        while(Iter < arr.length)
+            if(stk.isEmpty()) stk.add(arr[Iter++]);
+            else if(stk.get(stk.size() - 1) < arr[Iter]) stk.add(arr[Iter++]);
+            else stk.remove(stk.size() - 1);
 
+        int[] result = new int[stk.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stk.get(i);
+        }
         return result;
     }
 }
