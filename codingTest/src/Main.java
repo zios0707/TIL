e2e6bd6bd6eb80cac098c897262ccf3ffe8e682b
zios <khyunwook0707@gmail.com>
2023-10-10 20:23:51 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 세로 읽기
    
    그냥 for문 응용하면 됨.

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,30 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("Progra21Sremm3", 6, 12));
+        System.out.println(solution.solution("ihrhbakrfpndopljhygc",4, 2));
     }
 }
 
 
 class Solution {
     /*
-    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181905 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 뒤집기
+    /*  https://school.programmers.co.kr/learn/courses/30/lessons/181904 코딩테스트 연습 > 코딩 기초 트레이닝 > 세로 읽기
     /**                                                                                                            */
-    public String solution(String my_string, int s, int e) {
-        char[] charStr = my_string.toCharArray();
-        int Iter = e;
-        for (int i = s; i < Iter; i++) charStr = swap(charStr ,i ,Iter--);
-        return String.valueOf(charStr);
-    }
-
-    private char[] swap (char[] str, int index1, int index2) {
-        char tmp = str[index1];
-        str[index1] = str[index2];
-        str[index2] = tmp;
-        return str;
+    public String solution(String my_string, int m, int c) {
+        String result = "";
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            result += my_string.charAt(i);
+        }
+        return result;
     }
 }
