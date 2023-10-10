0c81e2895a1d45430dabe74172c7cd1a00f7e485
zios <khyunwook0707@gmail.com>
2023-10-10 17:27:24 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 9로 나눈 나머지
    
    솔직히 진짜 일일이 하나하나 나눗셈 했는데 문제 더 읽어보니까 더 편하게 할 수 있는 방법 있길래 그걸로 클-린하게 바꿈. 그리고 클래스 충돌 나길래 문제 저장 방식을 조금 바꿈.

--- codingTest/src/Programmers/L30N181914.java
+++ codingTest/src/Programmers/L30N181914.java
@@ -1,17 +1,15 @@
-public class Main {
+package Programmers;
+
+public class L30N181914 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("78720646226947352489"));
+        System.out.println(solution("78720646226947352489"));
     }
-}
 
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181914 코딩테스트 연습 > 코딩 기초 트레이닝 > 9로 나눈 나머지
     /**                                                                                                                */
-    public int solution(String number) {
+
+    private static int solution(String number) {
         int val = 0;
 
         for (char c: number.toCharArray()) val += Integer.valueOf(c - '0');
--- codingTest/src/Programmers/L30N181915.java
+++ codingTest/src/Programmers/L30N181915.java
@@ -2,21 +2,17 @@ package Programmers;
 
 public class L30N181915 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("cvsgiorszzzmrpaqpe", new int[]{16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7}));
+        System.out.println(solution("cvsgiorszzzmrpaqpe", new int[]{16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7}));
     }
-}               
 
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181915 코딩테스트 연습 > 코딩 기초 트레이닝 > 글자 이어 붙여 문자열 만들기
     /**                                                                                                                             */
-    public String solution(String my_string, int[] index_list) {
+
+    public static String solution(String my_string, int[] index_list) {
         String result = "";
         for (int i: index_list
-             ) {
+        ) {
             result += my_string.charAt(i);
         }
         System.out.println(result);
