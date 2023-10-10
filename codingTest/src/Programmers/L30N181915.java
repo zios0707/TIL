e3422ed82bc1ab3570e98608f482cdfaa517fee1
zios <khyunwook0707@gmail.com>
2023-10-10 17:07:02 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 글자 이어 붙여 문자열 만들기
    
    여태까지 푼 것들 따로 저장해야되나 싶어서 이제야 이렇게 디렉토리 밑으로 저장한다. 얍얍
    노가다로 풀음. 어떻게든 반례를 찾아서 해결 해 냈다.

--- codingTest/src/Programmers/L30N181915.java
+++ codingTest/src/Programmers/L30N181915.java
@@ -1,10 +1,12 @@
-public class Main {
+package Programmers;
+
+public class L30N181915 {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
         System.out.println(solution.solution("cvsgiorszzzmrpaqpe", new int[]{16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7}));
     }
-}
+}               
 
 
 class Solution {
