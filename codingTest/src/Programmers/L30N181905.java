d2f7aa2ad6852b4c5916311cdb59288fbe66af9f
zios <khyunwook0707@gmail.com>
2023-10-10 20:16:20 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 뒤집기
    
    swap 메서드 직접 구현해줌. 자바는 Call By Refrence가 안되는지라 저렇게 바뀐 값을 반환해주고 받는 형식으로 해줌. 그외 구조는 L30N181913 과 같을거임

--- codingTest/src/Programmers/L30N181905.java
+++ codingTest/src/Programmers/L30N181905.java
@@ -1,20 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
+package Programmers;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("Progra21Sremm3", 6, 12));
+        System.out.println(solution("Progra21Sremm3", 6, 12));
     }
-}
 
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181905 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 뒤집기
     /**                                                                                                            */
-    public String solution(String my_string, int s, int e) {
+
+    public static String solution(String my_string, int s, int e) {
         char[] charStr = my_string.toCharArray();
         int Iter = e;
         for (int i = s; i < Iter; i++) charStr = swap(charStr ,i ,Iter--);
