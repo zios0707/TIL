f9b055e4819ec4a7a3fe1cc151e835475dcfe032
zios <khyunwook0707@gmail.com>
2023-10-10 19:41:27 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 접미사 배열
    
    stream 기능을 좀 더 숙지해야겠다. list 객체에 있는 sort 메서드를 사용하여 사전순 정렬을 적용했다.

--- codingTest/src/Programmers/L30N181909.java
+++ codingTest/src/Programmers/L30N181909.java
@@ -1,19 +1,17 @@
+package Programmers;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class L30N181909 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("banana"));
+        System.out.println(solution("banana"));
     }
-}
 
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181909 코딩테스트 연습 > 코딩 기초 트레이닝 > 접미사 배열
     /**                                                                                                            */
+
     public String[] solution(String my_string) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < my_string.length(); i++) list.add(my_string.substring(i));
