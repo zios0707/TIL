61c25bcf66456b5c160c639f2050e1e3a3ebb0bb
zios <khyunwook0707@gmail.com>
2023-10-10 19:54:08 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열의 앞의 n글자
    
    간단히 substring 사용해서 풀음

--- codingTest/src/Programmers/L30N181907.java
+++ codingTest/src/Programmers/L30N181907.java
@@ -1,20 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+package Programmers;
 
-public class Main {
+public class L30N181907 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("ProgrammerS123", 11));
+        System.out.println(solution("ProgrammerS123", 11));
     }
-}
-
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181907 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열의 앞의 n글자
     /**                                                                                                            */
-    public String solution(String my_string, int n) {
+    public static String solution(String my_string, int n) {
         return my_string.substring(0, n);
     }
 }
