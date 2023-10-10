f9a0ecfcacb24ce2e6786e91d2f5fa7c648a37b5
zios <khyunwook0707@gmail.com>
2023-10-10 17:44:32 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 여러 번 뒤집기
    
    스왑 메서드가 있었으면 좀 더 쉽게 코드를 만들 수 있지 않았을까 싶음

--- codingTest/src/Programmers/L30N181913.java
+++ codingTest/src/Programmers/L30N181913.java
@@ -1,17 +1,15 @@
-public class Main {
+package Programmers;
+
+public class L30N181913 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("rermgorpsam", new int[][]{{2, 3}, {0, 7}, {5, 9}, {6, 10}}));
+        System.out.println(solution("rermgorpsam", new int[][]{{2, 3}, {0, 7}, {5, 9}, {6, 10}}));
     }
-}
 
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181913 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 여러 번 뒤집기
     /**                                                                                                                    */
-    public String solution(String my_string, int[][] queries) {
+
+    public static String solution(String my_string, int[][] queries) {
         char[] charString = my_string.toCharArray();
         for (int[] query:queries) {
             int j = query[1];
