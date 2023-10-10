a3bb298a9217e97e1c1229b954129ced430ddb7c
zios <khyunwook0707@gmail.com>
2023-10-10 19:14:28 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 부분 문자열 이어 붙여 문자열 만들기
    
    역시나 substring을 사용하여 간단히 풀음

--- codingTest/src/Programmers/L30N181911.java
+++ codingTest/src/Programmers/L30N181911.java
@@ -1,17 +1,15 @@
-public class Main {
+package Programmers;
+
+public class L30N181911 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution(new String[]{"progressive", "hamburger", "hammer", "ahocorasick"}, new int[][]{{0, 4}, {1, 2}, {3, 5}, {7, 7}}));
+        System.out.println(solution(new String[]{"progressive", "hamburger", "hammer", "ahocorasick"}, new int[][]{{0, 4}, {1, 2}, {3, 5}, {7, 7}}));
     }
-}
 
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181911 코딩테스트 연습 > 코딩 기초 트레이닝 > 부분 문자열 이어 붙여 문자열 만들기
     /**                                                                                                                                 */
-    public String solution(String[] my_strings, int[][] parts) {
+
+    public static String solution(String[] my_strings, int[][] parts) {
         String result = "";
 
         for (int i = 0; i < my_strings.length; i++) {
