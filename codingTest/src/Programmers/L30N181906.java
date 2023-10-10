2542ebe21ab6d4a307adb52259b146db64f8ea8d
zios <khyunwook0707@gmail.com>
2023-10-10 19:59:25 +0900

    // 프로그래머스 코테
    코딩테스트 연습 > 코딩 기초 트레이닝 > 접두사인지 확인하기
    
    접미사 확인처럼 메서드 사용해주면 해결.
    근데 이렇게 풀면 로직 제대로 이해 못하고 넘어간다고 생각할 수 있는데 로직 구현은 쉬운데 귀찮은 부분이 많아서 로직 구현은 c언어에서만 하고 메서드 활용 부분에서 자바로 득좀 보고 싶어서 그런거다. 단순히 귀찮아서 그런게 아님. 진짜 아님.

--- codingTest/src/Programmers/L30N181906.java
+++ codingTest/src/Programmers/L30N181906.java
@@ -1,20 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+package Programmers;
 
-public class Main {
+public class L30N181906 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.solution("banana", "ban"));
+        System.out.println(solution("banana", "ban"));
     }
-}
-
-
-class Solution {
     /*
     /*  https://school.programmers.co.kr/learn/courses/30/lessons/181906 코딩테스트 연습 > 코딩 기초 트레이닝 > 접두사인지 확인하기
     /**                                                                                                            */
-    public int solution(String my_string, String is_prefix) {
+    public static int solution(String my_string, String is_prefix) {
         return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
